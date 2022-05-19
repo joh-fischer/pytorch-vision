ef5721b9e312302ba5a01e462363ce17ff9af78d
Johannes Fischer <joh.fischer@outlook.com>
2022-05-19 14:35:23 +0200

    add shortcut connection

--- layers.py
+++ layers.py
@@ -17,6 +17,11 @@ class ResidualBlock(nn.Module):
         self.conv2_bn = nn.BatchNorm2d(out_channels)
 
         self.activation = nn.ReLU()
+        
+        if in_channels == out_channels:
+            self.shortcut = nn.Identity()
+        else:
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=padding)
 
     def forward(self, x):
         out = self.conv1(x)
@@ -25,36 +30,11 @@ class ResidualBlock(nn.Module):
 
         out = self.conv2(out)
         out = self.conv2_bn(out)
+        
+        x = self.shortcut(x)
+
         out = torch.add(out, x)
         out = self.activation(out)
 
         return out
 
-
-class DownsampleResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3):
-        super(DownsampleResidualBlock, self).__init__()
-
-        padding = (kernel_size // 2, kernel_size // 2)
-
-        self.conv1x1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=padding)
-
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
-        self.conv1_bn = nn.BatchNorm2d(out_channels)
-
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
-        self.conv2_bn = nn.BatchNorm2d(out_channels)
-
-        self.activation = nn.ReLU()
-
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.conv1_bn(out)
-        out = self.activation(out)
-
-        out = self.conv2(out)
-        out = self.conv2_bn(out)
-        out = torch.add(out, x)
-        out = self.activation(out)
-
-        return out
