d91d4c65d09f71a43754ffb778b94d72c71b1a7b
Johannes Fischer <joh.fischer@outlook.com>
2022-05-19 14:44:41 +0200

    fix layers

--- layers.py
+++ layers.py
@@ -4,24 +4,25 @@ import torch.nn as nn
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3):
-        super(ResidualBlock, self).__init__()
-
-        assert in_channels == out_channels, "Number of in and out channels must be equal."
+        super(ResidualBlock, self).__init__()      
+        
+        if in_channels == out_channels:
+            stride = 1
+            self.shortcut = nn.Identity()
+        else:
+            stride = 2
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride)
 
         padding = (kernel_size // 2, kernel_size // 2)
         
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride)
         self.conv1_bn = nn.BatchNorm2d(out_channels)
 
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride)
         self.conv2_bn = nn.BatchNorm2d(out_channels)
 
         self.activation = nn.ReLU()
-        
-        if in_channels == out_channels:
-            self.shortcut = nn.Identity()
-        else:
-            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=padding)
+
 
     def forward(self, x):
         out = self.conv1(x)
@@ -30,9 +31,8 @@ class ResidualBlock(nn.Module):
 
         out = self.conv2(out)
         out = self.conv2_bn(out)
-        
-        x = self.shortcut(x)
 
+        x = self.shortcut(x)
         out = torch.add(out, x)
         out = self.activation(out)
 
