5852aa570b1eabac422a1c3f025c5194cf4104ba
Johannes Fischer <joh.fischer@outlook.com>
2022-05-27 13:36:58 +0200

    refactor residual block

--- layers.py
+++ layers.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 
-class ResidualBlock(nn.Module):
+class ResBlock(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, kernel_size: int = 3):
         """
         Residual block as in https://arxiv.org/abs/1512.03385
@@ -12,37 +12,32 @@ class ResidualBlock(nn.Module):
             out_channels (int): number of output channels
             kernel_size: filter size for convolutions
         """
-        super(ResidualBlock, self).__init__()
+        super(ResBlock, self).__init__()
 
-        padding = (kernel_size // 2, kernel_size // 2)
-
-        if in_channels == out_channels:
-            stride = 1
-            self.shortcut = nn.Identity()
-        else:
-            stride = 2
-            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
-                                      padding=padding, stride=stride)
-        
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding="same")
         self.conv1_bn = nn.BatchNorm2d(out_channels)
 
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding="same")
         self.conv2_bn = nn.BatchNorm2d(out_channels)
 
         self.activation = nn.ReLU()
 
+        if in_channels == out_channels:
+            self.shortcut = nn.Identity()
+        else:
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), padding="same")
+
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.conv1_bn(out)
-        out = self.activation(out)
+        identity = self.shortcut(x)
 
-        out = self.conv2(out)
-        out = self.conv2_bn(out)
+        x = self.conv1(x)
+        x = self.conv1_bn(x)
+        x = self.activation(x)
 
-        x = self.shortcut(x)
-        print("adding ", x.shape, "and", out.shape)
-        out = torch.add(out, x)
-        out = self.activation(out)
+        x = self.conv2(x)
+        x = self.conv2_bn(x)
 
-        return out
+        x = torch.add(x, identity)
+        x = self.activation(x)
+
+        return x
