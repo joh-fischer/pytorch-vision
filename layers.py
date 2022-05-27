8d8209d7f2780af218006406464b73237836851b
Johannes Fischer <joh.fischer@outlook.com>
2022-05-27 15:23:39 +0200

    make res block work

--- layers.py
+++ layers.py
@@ -14,30 +14,34 @@ class ResidualBlock(nn.Module):
         """
         super(ResidualBlock, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding="same")
+        padding = (kernel_size // 2, kernel_size // 2)
+
+        if in_channels == out_channels:
+            stride = (1, 1)
+            self.shortcut = nn.Identity()
+        else:
+            stride = (2, 2)
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), padding=(0, 0), stride=stride)
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride)
         self.conv1_bn = nn.BatchNorm2d(out_channels)
 
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding="same")
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
         self.conv2_bn = nn.BatchNorm2d(out_channels)
 
         self.activation = nn.ReLU()
 
-        if in_channels == out_channels:
-            self.shortcut = nn.Identity()
-        else:
-            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), padding="same")
-
     def forward(self, x):
         identity = self.shortcut(x)
-
+        print("identity:", identity.shape)
         x = self.conv1(x)
         x = self.conv1_bn(x)
         x = self.activation(x)
 
         x = self.conv2(x)
         x = self.conv2_bn(x)
-
-        x = torch.add(x, identity)
+        print("x:", x.shape)
+        x += identity
         x = self.activation(x)
 
         return x
